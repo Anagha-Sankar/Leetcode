1857f353887c31674054ab72b147b684d686b6f6
Anagha Sankar <anaghasankar3@gmail.com>
2022-06-09 14:52:58 +0530

    Initial commit

---
+++ first-bad-version.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+ bool isBadVersion(int version);
+
+
+int firstBadVersion(int n) {
+        int ans = n;
+        int first =1;
+        int last = n;
+        while(first<=last){
+            int mid = first+((last-first)/2);
+            if(isBadVersion(mid)){
+                ans = mid;
+                last = mid-1;
+            }
+            else{
+                first = mid+1;
+            }
+        }
+        return ans;
+}
+
